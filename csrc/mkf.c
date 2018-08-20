e48d39e9734c1947bdaf5971b4a413d60a7de929
Iru Cai <mytbk920423@gmail.com>
2018-08-20 10:28:06 +0800

    rename mkf_data array to mkf_stdata

--- csrc/mkf.c
+++ csrc/mkf.c
@@ -1,9 +1,11 @@
+#include <windows.h>
+
 struct mkf
 {
 	HANDLE handle;
 	char *data;
 };
-struct mkf mkf_data[16] = { {-1, NULL} }; // 0x4762f4
+struct mkf mkf_stdata[16] = { {-1, NULL} }; // 0x4762f4
 
 uint32_t load_mkf(const char *fn)
 {
@@ -11,23 +13,23 @@ uint32_t load_mkf(const char *fn)
 	int32_t x;
 	int sz;
 
-	if (mkf_data[0].handle == -1)
-		memset(mkf_data, 0, sizeof(mkf_data));
+	if (mkf_stdata[0].handle == -1)
+		memset(mkf_stdata, 0, sizeof(mkf_stdata));
 
 	HANDLE fhdl = CreateFileA(fn, 0x80000000, 0, 0, 3, 128, 0);
 	if (fhdl == -1)
 		return -1;
 
-	for (i = 0; mkf_data[i].handle != 0; i++)
+	for (i = 0; mkf_stdata[i].handle != 0; i++)
 		;
 
-	mkf_data[i].handle = fhdl;
+	mkf_stdata[i].handle = fhdl;
 	ReadFile(fhdl, &x, 4, &sz, NULL);
 	int filesz = GetFileSize(fhdl, NULL);
 	int t = filesz - x;
 	SetFilePointer(fhdl, x, 0, 0);
-	mkf_data[i].data = (char*)malloc(t);
-	ReadFile(fhdl, mkf_data[i].data, t, &sz, NULL);
+	mkf_stdata[i].data = (char*)malloc(t);
+	ReadFile(fhdl, mkf_stdata[i].data, t, &sz, NULL);
 	return i;
 }
 
@@ -36,11 +38,11 @@ char * read_mkf(int mkf_idx, int a1, char *buf, int *bufsize) //0x00450441
 	uint32_t data[4];
 	int sz;
 
-	HANDLE hdl = mkf_data[mkf_idx].handle;
+	HANDLE hdl = mkf_stdata[mkf_idx].handle;
 	if (hdl == 0)
 		return 0;
 
-	SetFilePointer(hdl, mkf_data[mkf_idx].data[a1*4], 0, 0);
+	SetFilePointer(hdl, mkf_stdata[mkf_idx].data[a1*4], 0, 0);
 	ReadFile(hdl, data, 16, &sz, NULL);
 
 	if (buf == NULL) {
