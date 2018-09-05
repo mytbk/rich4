ee84d9b9b84b35eab986e3feced18ab926412a0a
Iru Cai <mytbk920423@gmail.com>
2018-09-05 23:44:20 +0800

    fcn_4562a5, fcn_455c52

--- csrc/drawing.c
+++ csrc/drawing.c
@@ -1,5 +1,114 @@
 #include <windows.h>
 #include "player_info.h"
+#include "data_struct.h"
+
+struct st st_46caec;
+
+int fcn_455c52(uint32_t a0, uint32_t a1, uint16_t *a2, struct st * a3, int a4, int a5, int a6)
+{
+	a4 -= (int16_t)a3->f4;
+	a5 -= (int16_t)a3->f6;
+	if (a6 != 1) {
+		if (a4 >= a0 ||
+				a4 + (uint16_t)a3->f0 <= 0 ||
+				a5 >= a1 ||
+				a5 + (uint16_t)a3->f2 <= 0)
+			return 1;
+
+		[ebp-4] = 0;
+		[ebp-8] = 0;
+		[ebp-12] = (uint16_t)a3->f0;
+		[ebp-16] = (uint16_t)a3->f2;
+
+		if (a4 < 0) {
+			[ebp-4] -= a4;
+			[ebp-12] += a4;
+			a4 = 0;
+		} else {
+			eax = [ebp-12] + a4 - a0;
+			if (eax > 0) {
+				[ebp-12] -= eax;
+			}
+		}
+
+		if (a5 < 0) {
+			[ebp-8] -= a5;
+			[ebp-16] += a5;
+			a5 = 0;
+		} else {
+			eax = [ebp-16] + a5 - a1;
+			if (eax > 0) {
+				[ebp-16] -= eax;
+			}
+		}
+	} else {
+		if (a4 >= dw_4861c0
+				|| a4 + (uint16_t)a3->f0 <= dw_4861b8
+				|| a5 >= dw_4861c4
+				|| a5 + (uint16_t)a3->f2 <= dw_4861bc)
+			return 1;
+
+		[ebp - 4] = 0;
+		[ebp - 8] = 0;
+		[ebp - 12] = (uint16_t)a3->f0;
+		[ebp - 16] = (uint16_t)a3->f2;
+
+		if (a4 < dw_4861b8) {
+			eax = dw_4861b8 - a4;
+			a4 = dw_4861b8;
+			[ebp - 4] += eax;
+			[ebp - 12] -= eax;
+		} else {
+			eax = [ebp - 12] + a4 - dw_4861c0;
+			if (eax > 0) {
+				[ebp - 12] -= eax;
+			}
+		}
+
+		if (a5 < dw_4861bc) {
+			eax = dw_4861bc - a5;
+			a5 = dw_4861bc;
+			[ebp - 8] += eax;
+			[ebp - 16] -= eax;
+		} else {
+			eax = [ebp  - 16] + a5 - dw_4861c4;
+			if (eax > 0) {
+				[ebp - 16] -= eax;
+			}
+		}
+
+	}
+
+	eax = (uint16_t)a3->f0 * [ebp-8] + [ebp-4];
+	esi = &a3->f8[eax];
+	[ebp - 4] = ((uint16_t)a3->f0 - [ebp - 12]) * 2;
+	[ebp - 8] = (a0 - [ebp-12]) * 2;
+	edi = (a5 * a0 + a4) * 2 + a2;
+
+	edx = [ebp - 12];
+	ebx = [ebp - 16];
+	do {
+		ecx = edx;
+		do {
+			ax = [esi];
+			(uint16_t*)esi ++;
+			if (ax != 0) {
+				*edi = ax;
+			}
+			(uint16_t*)edi ++;
+		} while (--ecx);
+
+		esi += [ebp - 4];
+		edi += [ebp - 8];
+	} while (--ebx);
+
+	return 0;
+}
+
+void fcn_4562a5(struct st * a0, struct st * a1, int a2, int a3)
+{
+	fcn_455c52((uint16_t)a0->f0, (uint16_t)a0->f2, a0->f8, a1, a2, a3, 0);
+}
 
 void fcn_417191(int a0)
 {
@@ -32,22 +141,22 @@ void fcn_417191(int a0)
 		ebx = 4;
 
 	IDirectDrawSurface_Lock(pddrawsf1, 0, &sfdesc1, 1, 0);
-	w_46caec = sfdesc1.DUMMYUNIONNAME1.lPitch / 2;
+	st_46caec.f0 = sfdesc1.DUMMYUNIONNAME1.lPitch / 2;
 	dw_46caf4 = sfdesc1.lpSurface;
 
 	edx = dw_48bdd4 + ebx;
 	edx = edx * 12 + dw_48be04 + 12;
 
-	fcn.004562a5(0x46caec, edx, dw_475284, dw_475288);
+	fcn_4562a5(&st_46caec, edx, dw_475284, dw_475288);
 
 	eax = players[current_player].traffic_method & 3;
 	switch (eax) {
 		case 0:
 			eax = dw_475288 + 0x1a;
 			if (players[current_player].days_stopping == 0) {
-				fcn.004562a5(0x46caec, dw_48be04+0x60, dw_475284+7, eax);
+				fcn_4562a5(&st_46caec, dw_48be04+0x60, dw_475284+7, eax);
 			} else {
-				fcn.004562a5(0x46caec, dw_48be04+0x54, dw_475284+8, eax);
+				fcn_4562a5(&st_46caec, dw_48be04+0x54, dw_475284+8, eax);
 			}
 			break;
 		case 1:
@@ -57,11 +166,11 @@ void fcn_417191(int a0)
 					eax_1 = ebx * 19 + dw_475288 + 0x10;
 					edx = (ebx * 2 + 7) * 12;
 					eax_2 = dw_48be04 + 0xc + edx;
-					fcn.004562a5(0x46caec, eax_2, dw_475284 + 7, eax_1);
+					fcn_4562a5(&st_46caec, eax_2, dw_475284 + 7, eax_1);
 				} else {
 					eax_1 = ebx*19 + dw_475288 + 0x10;
 					eax_2 = (ebx * 2 + 6) * 12 + dw_48be04 + 0xc;
-					fcn.004562a5(0x46caec, eax_2, dw_475284 + 8, eax_1);
+					fcn_4562a5(&st_46caec, eax_2, dw_475284 + 8, eax_1);
 				}
 			}
 			break;
@@ -72,12 +181,12 @@ void fcn_417191(int a0)
 					a4 = ebx * 16 + dw_475288 + 9;
 					a3 = dw_475284 + 7;
 					a2 = (ebx * 2 + 7) * 12 + dw_48be04 + 0xc;
-					fcn.004562a5(0x46caec, a2, a3, a4);
+					fcn_4562a5(&st_46caec, a2, a3, a4);
 				} else {
 					a4 = ebx * 16 + dw_475288 + 9;
 					a3 = dw_475284 + 8;
 					a2 = (ebx * 2 + 6) * 12 + dw_48be04 + 0xc;
-					fcn.004562a5(0x46caec, a2, a3, a4);
+					fcn_4562a5(&st_46caec, a2, a3, a4);
 				}
 			}
 			break;
@@ -86,7 +195,7 @@ void fcn_417191(int a0)
 	}
 	IDirectDrawSurface_Unlock(pddrawsf1, NULL);
 	GetCursorPos_250(&r0);
-	w_46caec = 0x280;
+	st_46caec.f0 = 640;
 }
 
 void IntersectRect_4cd(RECT *r0)
