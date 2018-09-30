746b537e33a4c840e43ae852134d9a5e7cccfdcd
Iru Cai <mytbk920423@gmail.com>
2018-09-30 20:33:00 +0800

    stocks

---
+++ csrc/stock.h
@@ -0,0 +1,20 @@
+#include <stdint.h>
+
+typedef struct
+{
+	const char * name_ptr;
+	uint16_t f4;
+	uint8_t f6;
+	uint8_t f7;
+	uint16_t f8;
+	uint16_t f10;
+	float f12;
+	float f16;
+	float f20;
+	float f24;
+	float f28;
+	uint32_t f32;
+} stock_info;
+
+extern stock_info stocks[]; /* 0x496980 */
+extern stock_info game_stocks[]; /* 0x47f072 */
