d2415c282518bfc0ff72bcbcdb1be6c37208efab
Iru Cai <mytbk920423@gmail.com>
2018-11-16 22:31:25 +0800

    add misc.h, license

---
+++ csrc/misc.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C)  2018 Iru Cai <mytbk920423@gmail.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+void playmid_4549cf(int x);
+void close_mid_454acb(void);
+void play_avi(const char *fn, RECT *r, int x);
+void playmid_454d2c(void);
