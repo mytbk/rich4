06c370edeb1944a9743a4a87fb92ea61b379b727
Iru Cai <mytbk920423@gmail.com>
2021-05-10 23:32:48 +0800

    use stocks data C code

--- asm/stocks_data.c
+++ asm/stocks_data.c
@@ -1,4 +1,4 @@
-#include "stock.h"
+#include "stocks.h"
 
 stock_info game_stocks[96] = {
 	{ "\xa4\xa4\xb0\xea\xab\x48\xb0\x55", 1, 0, 0, 10000, 0, 100.0, 100.0, 100.0, 1.000000, 0.0, 0 }, /* 中国信托 */
